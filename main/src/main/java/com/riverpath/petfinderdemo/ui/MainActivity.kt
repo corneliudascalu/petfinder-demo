ef6478afbba2bee3d0c6a2e5489d77b961426086
Corneliu Dascălu <corneliu.dascalu@qredo.com>
2024-01-22 20:18:44 +0200

    Build a search bar UI

--- main/src/main/java/com/riverpath/petfinderdemo/ui/MainActivity.kt
+++ main/src/main/java/com/riverpath/petfinderdemo/ui/MainActivity.kt
@@ -3,13 +3,35 @@ package com.riverpath.petfinderdemo.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.riverpath.petfinderdemo.ui.ui.theme.PetfinderDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +44,77 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PetfinderUI()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun PetfinderUI(modifier: Modifier = Modifier) {
+    val query = remember {
+        mutableStateOf("")
+    }
+    Scaffold(
+        modifier = modifier,
     )
+    {
+        SearchBar(
+            placeholder = { Text(text = "Find a pet") },
+            query = query.value,
+            onQueryChange = { q -> query.value = q },
+            onSearch = {},
+            active = true,
+            onActiveChange = {},
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = "",
+                )
+            },
+            trailingIcon = {
+                Icon(imageVector = Icons.Rounded.Clear, contentDescription = "")
+            },
+            colors = SearchBarDefaults.colors(containerColor = Color.Transparent),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(it)
+                .background(
+                    color = MaterialTheme.colorScheme.background,
+                    shape = CircleShape.copy(all = CornerSize(24.dp))
+                )
+        ) {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .fillMaxWidth(),
+                    text = "test",
+                    maxLines = 3,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Authenticate")
+                }
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PetfinderDemoTheme {
-        Greeting("Android")
+        PetfinderUI()
     }
 }
